155d26ab67f1124a048b56c8c4bc73e1287de464
dawid.rezmer <dawid.rezmer@profil-software.com>
2023-10-28 18:56:21 +0200

    Created models

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
-# Create your models here.
+class RoleTypes(models.TextChoices):
+    ADMIN = 'admin'
+    CLIENT = 'client'
+
+class GenderTypes(models.TextChoices):
+    MALE = 'male'
+    FEMALE = 'female'
+    OTHER = 'other'
+
+class Client(models.Model):
+    first_name = models.CharField(max_length=255)
+    second_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, unique=True)
+    role = models.CharField(max_length=255, choices=RoleTypes.choices, default=RoleTypes.CLIENT)
+    gender = models.CharField(max_length=255, choices=GenderTypes.choices, default=GenderTypes.OTHER)
+    street_address = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.email
+
+class Package(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    price = models.CharField(max_length=255)
+    movies = models.ManyToManyField('Movie', related_name='packages')
+
+    def __str__(self):
+        return self.name
+
+class  Subscription(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    package = models.ForeignKey(Package, on_delete=models.CASCADE)
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+    def __str__(self):
+        return self.client.email + ' ' + self.package.name
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255, unique=True)
+    description = models.CharField(max_length=255)
+    genre = models.CharField(max_length=255)
+    release_year = models.PositiveSmallIntegerField()
+    rating = models.DecimalField(max_digits=3, decimal_places=1)
+
+    def __str__(self):
+        return self.name
+
